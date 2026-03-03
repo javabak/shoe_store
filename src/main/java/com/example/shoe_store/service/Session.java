b8847b1ee09c58d66c7edcd2cdc5fb7447382363
alim <asas@gmail.com>
2026-03-03 19:13:51 +0300

    refactored session class

--- src/main/java/com/example/shoe_store/service/Session.java
+++ src/main/java/com/example/shoe_store/service/Session.java
@@ -1,24 +1,3 @@
-//package com.example.shoe_store.service;
-//
-//import com.example.shoe_store.model.User;
-//
-//public class Session {
-//
-//    private static User currentUser;
-//
-//    public static void setCurrentUser(User user) {
-//        currentUser = user;
-//    }
-//
-//    public static User getCurrentUser() {
-//        return currentUser;
-//    }
-//
-//    public static void logout() {
-//        currentUser = null;
-//    }
-//}
-
 package com.example.shoe_store.service;
 
 import com.example.shoe_store.model.User;
